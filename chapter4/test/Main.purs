c03e9a9a0dec53d293721162fd3440ee6892b5e4
Lee Henson <lee.m.henson@gmail.com>
2016-12-07 21:36:17 +0000

    wip

--- chapter4/test/Main.purs
+++ chapter4/test/Main.purs
@@ -1,16 +1,48 @@
-module Test.Main where
+module Test.Main (main) where
+
+-- kudos http://konkle.us/purescript-for-front-end-developers/
 
 import Prelude
 
+import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Data.Path (root)
-import Data.Foldable (for_)
-import FileOperations (allFiles)
-import Exercises (squares)
+import Control.Monad.Eff.Console (CONSOLE)
 
-main :: Eff (console :: CONSOLE) Unit
-main =
-  logShow squares [1.0, 2.0]
-  for_ (allFiles root) logShow
+import Test.Unit (suite, test)
+import Test.Unit.Assert (equal)
+import Test.Unit.Console (TESTOUTPUT)
+import Test.Unit.Main (runTest)
+
+import Exercises (noNegs, squares)
+
+main :: Eff (console :: CONSOLE, testOutput :: TESTOUTPUT, avar :: AVAR) Unit
+main = runTest do
+  suite "Exercises" do
+    suite "squares" do
+      test "[]" do
+        equal (squares []) []
+
+      test "[1.0]" do
+        equal (squares [1.0]) [1.0]
+
+      test "[1.0, 2.0]" do
+        equal (squares [1.0, 2.0]) [1.0, 4.0]
+
+    suite "noNegs" do
+      test "[]" do
+        equal (noNegs []) []
+
+      test "[1.0]" do
+        equal (noNegs [1.0]) [1.0]
+
+      test "[-1.0]" do
+        equal (noNegs [-1.0]) []
+
+-- import Data.Path (root)
+-- import Data.Foldable (for_)
+-- import FileOperations (allFiles)
+
+-- main :: Eff (console :: CONSOLE) Unit
+-- main =
+--   for_ (allFiles root) logShow
 
